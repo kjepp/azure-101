bfc86264178fb0d4e885a601634998d038322324
Magnus Moen <magnus.moen@computas.com>
2020-02-12 10:37:58 +0100

    framework

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SpeedTest;
+using SpeedTest.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Azure Workshop!");
+
+            var client   = new SpeedTestClient();
+            var settings = client.GetSettings();
+        }
+    }
+}
