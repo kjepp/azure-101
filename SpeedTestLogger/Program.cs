162e4bafe136c6e198fc5b9493e2fa1f4ab5f2c1
Magnus Moen <magnus.moen@computas.com>
2020-02-12 11:35:33 +0100

    My First Dotnet Program Huzzah

--- SpeedTestLogger/Program.cs
+++ SpeedTestLogger/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using SpeedTest;
 using SpeedTest.Models;
+using SpeedTestLogger.Models;
 
-namespace SpeedTestLogger
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello from Azure Workshop!");
+namespace SpeedTestLogger {
+    class Program {
+        static void Main(string[] args) {
 
-            var client   = new SpeedTestClient();
-            var settings = client.GetSettings();
+            Console.WriteLine("Hi SpeedTestLogger");
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            var testData = runner.RunSpeedTest();
+
+            var results = new TestResult {
+                SessionId = new Guid(),
+                User = config.UserId,
+                Device = config.LoggerId,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
         }
     }
 }
